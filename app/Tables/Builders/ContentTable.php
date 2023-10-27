bcbfb89956b8c4b2b93ce17ce7d9d84326809fc1
Alain-Kay <kazadikayumba@gmail.com>
2023-10-27 14:43:30 +0200

    Added fields

--- app/Tables/Builders/ContentTable.php
+++ app/Tables/Builders/ContentTable.php
@@ -13,9 +13,11 @@ class ContentTable implements Table
     public function query(): Builder
     {
         return Content::selectRaw('
-        contents.content_id, contents.content_title, contents.content_body, 
-        contents.content_category_id, contents.author_id
-    ');
+                        contents.content_id, contents.content_title, contents.content_body, 
+                        contents.content_category_id, contents.author_id,
+                        contents.published_date, contents.content_type, 
+                        contents.content_status, contents.featured_image_url,
+                    ');
     }
     public function templatePath(): string
     {
